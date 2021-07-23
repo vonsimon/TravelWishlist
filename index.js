5e4f03730a0b6a519974957aa81cd3c785332bc2
Simon <vonsimon2@gmail.com>
2021-07-23 13:49:17 +0200

    with Jorges solution

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 import express from 'express';
+import countries from './countries.js'
 
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.get("/", (req, res) => res.send("Hello World"));
+// RESTful API 
+const getAllCountries = (req, res) => {
+    // get data from somewhere else
+    res.json(countries)
+}
+// res.send() set the content-type header to text/html, res.json() sets the content-type header to application/json
+
+
+app.get("/api/countries", getAllCountries);
+app.post('/', (req, res) => res.send('POST'));
 
 app.listen(port, () => console.log(`Server listening at http://localhost:${port}`));
